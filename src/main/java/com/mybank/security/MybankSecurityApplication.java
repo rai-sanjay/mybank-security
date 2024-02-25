21d77ed34912f24851758af6996e15318d13745e
rai-sanjay <rai.sanjay2015@outlook.com>
2024-02-25 18:32:42 +0530

    project setup

---
+++ src/main/java/com/mybank/security/MybankSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.mybank.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybankSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybankSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybank/security/MybankSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybank.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybankSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
