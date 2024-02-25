a566a2fc4aa99f18983c0bbe6e4e9f959961e0ac
rai-sanjay <rai.sanjay2015@outlook.com>
2024-02-25 19:49:42 +0530

    Secured loan,balance,card and account , unsecured contact and notice APIs

--- src/main/java/com/mybank/config/MyBankSecurityConfig.java
+++ src/main/java/com/mybank/config/MyBankSecurityConfig.java
@@ -9,12 +9,26 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class MyBankSecurityConfig {
 
-	@Bean
+	// Secures all endpoints invoked from form and basic security
+/*	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		System.out.println("Created own bean of security filter chain : CALLED AT APPLICATION STARTUP TIME");
 		httpSecurity.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
 		httpSecurity.formLogin(Customizer.withDefaults());
 		httpSecurity.httpBasic(Customizer.withDefaults());
+		return httpSecurity.build();
+
+	}*/
+	
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+		.authorizeHttpRequests(
+				(requests) -> requests.requestMatchers("/api/account", "/api/balance", "/api/loan", "/api/card")
+						.authenticated() // APIs need to be authenticated -- can use RegEx also
+		.requestMatchers("/api/notice", "/api/contact").permitAll()) // does not needs authentication
+		.formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults()); // source of request - Rest and HTML form based login
+
 		return httpSecurity.build();
 
 	}
