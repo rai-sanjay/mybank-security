c6e216228088bf2e23c2699d6918e913cc657b1f
rai-sanjay <rai.sanjay2015@outlook.com>
2024-03-02 17:25:11 +0530

    implemented CORS GLOBALLY

--- src/main/java/com/mybank/config/MyBankSecurityConfig.java
+++ src/main/java/com/mybank/config/MyBankSecurityConfig.java
@@ -1,20 +1,18 @@
 package com.mybank.config;
 
-import javax.sql.DataSource;
+import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class MyBankSecurityConfig {
@@ -44,7 +42,21 @@ public class MyBankSecurityConfig {
 	
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf((csrf) -> csrf.disable())
+		
+		httpSecurity.cors((cors ) -> cors.configurationSource(new CorsConfigurationSource() {
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.setAllowCredentials(true);
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setMaxAge(3600L);
+                return config;
+			}
+		}))
+		
+		.csrf((csrf) -> csrf.disable())
 		.authorizeHttpRequests(
 				(requests) -> requests.requestMatchers("/api/account", "/api/balance", "/api/loan", "/api/card","/api/users")
 						.authenticated() // APIs need to be authenticated -- can use RegEx also
